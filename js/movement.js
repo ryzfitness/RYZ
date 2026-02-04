2ee2e8a09e95ae1c29254bfec3b5fe0f83145063
RYZ <104311100+ryzfitness@users.noreply.github.com>
2026-02-04 14:32:26 -0500

    Add movement section to the app

---
+++ js/movement.js
@@ -0,0 +1,4 @@
+document.getElementById("app").innerHTML = `
+  <img class="banner" src="assets/banners/movement.jpg">
+  <div class="card"><h2>Movement</h2><p>Workout library coming soon.</p></div>
+`;
