77afa43e3fcb28d6e1097b52b9afc96edefaf857
RYZ <104311100+ryzfitness@users.noreply.github.com>
2026-02-04 14:56:03 -0500

    Create insights.js

---
+++ js/insights.js
@@ -0,0 +1,4 @@
+document.getElementById("app").innerHTML = `
+  <img class="banner" src="assets/banners/insights.jpg">
+  <div class="card"><h2>Insights</h2><p>Devotionals and resources.</p></div>
+`;
