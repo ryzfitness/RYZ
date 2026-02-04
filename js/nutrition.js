583308f603cf9f36686d079d1dfcb4b7a42df7c5
RYZ <104311100+ryzfitness@users.noreply.github.com>
2026-02-04 14:32:41 -0500

    Add nutrition section to the app

---
+++ js/nutrition.js
@@ -0,0 +1,4 @@
+document.getElementById("app").innerHTML = `
+  <img class="banner" src="assets/banners/nutrition.jpg">
+  <div class="card"><h2>Nutrition</h2><p>Recipes and meal planning.</p></div>
+`;
